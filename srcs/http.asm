ed519b3ad989916a9bccc521a068085146897023
DylanC-Code <dylanc.code@gmail.com>
2025-06-26 23:51:56 +0200

    feat(http): add http_handle_request function to http.asm and update main.asm

---
+++ srcs/http.asm
@@ -0,0 +1,7 @@
+BITS 64
+
+section .text
+
+global http_handle_request
+
+http_handle_request:
--- srcs/main.asm
+++ srcs/main.asm
@@ -7,13 +7,16 @@ extern socket_build
 extern socket_bind
 extern socket_listen
 extern socket_accept_connection
+extern http_handle_request
 extern utils_exit
 
 ; R12 = Main socket fd (défini dans socket_build)
+; R13 = Client socket fd
 
 _start:
-    call socket_build
-    call socket_bind
-    call socket_listen
-    call socket_accept_connection
-    call utils_exit
+    call    socket_build
+    call    socket_bind
+    call    socket_listen
+    call    socket_accept_connection
+    call    http_handle_request
+    call    utils_exit
