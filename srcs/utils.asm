7a87db7ff37c5cb0d9ae80a2936f0fcf226c2453
DylanC-Code <dylanc.code@gmail.com>
2025-06-29 23:22:58 +0200

    feat(utils): add ft_strlen function to calculate string length

--- srcs/utils.asm
+++ srcs/utils.asm
@@ -4,10 +4,38 @@ BITS 64
 
 
 section .text
-global utils_exit
+global exit_program
+global ft_strlen
 
 
-utils_exit:
+; =============================================================================
+; ft_strlen
+; -----------------------------------------------------------------------------
+; Calcule la longueur d’une chaîne terminée par \0 (comme strlen en C).
+; Entrée : rdi = adresse de la chaîne
+; Sortie : rax = longueur (hors \0)
+; =============================================================================
+ft_strlen:
+    xor     rax, rax
+
+.loop:
+    cmp     BYTE [rdi + rax], 0
+    je      .ft_strlen_end
+    inc     rax
+    jmp     .loop
+
+.ft_strlen_end:
+    ret
+
+
+; =============================================================================
+; exit_program
+; -----------------------------------------------------------------------------
+; Termine proprement le programme avec un code de retour 0.
+; =============================================================================
+exit_program:
     mov     rax, SYS_EXIT
     xor     rdi, rdi
     syscall
+
+
