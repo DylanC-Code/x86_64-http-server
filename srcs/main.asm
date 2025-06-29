d70afbc1744a4252dc56713955337b8952db981e
DylanC-Code <dylanc.code@gmail.com>
2025-06-30 00:11:22 +0200

    feat(fork): concurrency implemented

--- srcs/main.asm
+++ srcs/main.asm
@@ -1,15 +1,18 @@
+%include "constants.asm"
+
 BITS 64
 
 section .text
 global _start
+global handle_connection
 
 extern socket_build
 extern socket_bind
 extern socket_listen
 extern socket_accept_connection
-extern socket_close_connection
+extern socket_close_client_connection
 extern http_handle_request
-extern utils_exit
+extern exit_program
 
 ; R12 = Main socket fd (défini dans socket_build)
 ; R13 = Client socket fd
@@ -18,8 +21,25 @@ _start:
     call    socket_build
     call    socket_bind
     call    socket_listen
+    call    handle_connection
+
+handle_connection:
     call    socket_accept_connection
+    call    create_child
     call    http_handle_request
-    call    socket_close_connection
-    ; jmp     socket_accept_connection
-    call    utils_exit
+    call    socket_close_client_connection
+    call    exit_program
+
+create_child:
+    mov     rax, SYS_FORK
+    syscall
+
+    cmp     rax, 0
+    jne     .in_parent
+    ret
+
+.parent:
+    call    socket_close_client_connection
+    jmp     handle_connection
+
+
